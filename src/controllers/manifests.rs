0f883fc7dca643b8804a470c9ebecf4a01010304
l4p1n <l4p1n@l4p1n.ch>
2022-12-11 10:41:35 +0100

    Refactor the proxy manifest file writing in the dedicated function

--- src/controllers/manifests.rs
+++ src/controllers/manifests.rs
@@ -3,9 +3,9 @@ use std::os::unix::prelude::MetadataExt;
 use axum::{response::IntoResponse, extract::{Path, BodyStream, State}, TypedHeader, headers, http::{StatusCode, self}, body::{StreamBody, BoxBody}, debug_handler};
 use tokio::io::AsyncWriteExt;
 use tokio_util::io::ReaderStream;
-use tracing::{info, warn};
+use tracing::{info, warn, debug};
 
-use crate::{data::{helpers::{reject_invalid_container_refs, RegistryPathsHelper, reject_invalid_tags_refs}, manifests::{Manifest, ManifestMetadata}}, ApplicationState, docker_client::client::DockerClientError};
+use crate::{data::{helpers::{reject_invalid_container_refs, RegistryPathsHelper, reject_invalid_tags_refs}, manifests::{Manifest, ManifestMetadata, ManifestContentSources}}, ApplicationState, docker_client::client::DockerClientError};
 use crate::controllers::RegistryHttpResult;
 
 use super::RegistryHttpError;
@@ -28,7 +28,7 @@ pub async fn upload_manifest(
     );
 
     info!("Saving manifest");
-    manifest.save_manifest(&mut body).await?;
+    manifest.save_manifest((&mut body).into()).await?;
     info!("Saving metadata");
     manifest.save_manifest_metadata(&content_type.to_string()).await?;
 
@@ -36,7 +36,7 @@ pub async fn upload_manifest(
         StatusCode::CREATED,
         [
             ("Location", format!("/v2/{}/manifests/{}", container_ref, manifest_ref)),
-            ("Docker-Content-Digest", format!("sha256:{}", manifest.docker_hash()?))
+            ("Docker-Content-Digest", manifest.docker_hash()?.clone())
         ]
     ).into_response())
 }
@@ -87,10 +87,43 @@ pub async fn proxy_fetch_manifest(
     // TODO: Rearrange code to support offline proxying, that is if the upstream proxy did send 429 or any 5xx HTTP code
     let client = app.docker_clients.get_client(&container_ref).await?;
     info!("Querying upstream HEAD to fetch the most manifest related to the tag");
-    let manifest_head_response = client.query_manifest(&manifest_ref, true).await;
-    let proxy_manifest_head = match manifest_head_response {
-        Ok(proxy_response) => proxy_response,
+    
+    let (proxy_hash, content_length, content_type) = match client.query_manifest(&manifest_ref, true).await {
+        // The ideal case: the server returns a 200 on the HEAD HTTP request
+        Ok(proxy_response_head) => {
+            info!("Upstream returned 200 on the HEAD. Checking for cached hash file {}", proxy_response_head.hash);
 
+            // Check if we have the same copy of the manifest somewhere in our files before sending a GET request
+            // to the upstream respository.
+            let proxy_manifest_hash_path = RegistryPathsHelper::manifest_path(&app.conf.proxy_storage, &container_ref, &proxy_response_head.hash);
+            if !proxy_manifest_hash_path.is_file() {
+                info!("File does not exist. Querying and caching the upstream manifest");
+
+                tokio::fs::create_dir_all(&proxy_manifest_hash_path.parent().unwrap()).await?;
+                let proxy_manifest_meta_hash_path = RegistryPathsHelper::manifest_meta(&app.conf.proxy_storage, &container_ref, &proxy_response_head.hash);
+                tokio::fs::create_dir_all(proxy_manifest_meta_hash_path.parent().unwrap()).await?;
+                // We don't have the manifest, GET the manifest referenced by the hash sent by the server
+                // and dump it into a file in our file system, no matter the original client request method.
+                //
+                // This time, if an error occurred, we don't care about the status code. The only reasons a registry would send
+                // something other than a 200 is either rate limiting or server errors.
+                //
+                // Instead of bailing out, we could consider sending a stale version of the manifest. Later.
+                let mut proxy_manifest = client.query_manifest(&proxy_response_head.hash, false).await?;
+                let mut manifest_file = Manifest::new(&app.conf.proxy_storage, &app.conf.temporary_registry_storage, &container_ref, &manifest_ref);
+
+                // And write all the things. The function will be in charge of writing the docker image manifest and its
+                // related metadata, while making sure to not do stupid stuff such as overwriting the hash file with an
+                // empty version of itself.
+                manifest_file.save_manifest((&mut proxy_manifest.raw_response).into()).await?;
+            } else {
+                info!("Manifest is already cached");
+            }
+
+            (proxy_response_head.hash, proxy_response_head.content_length, proxy_response_head.content_type)
+        },
+
+        // Not ideal but easy to deal with: 404 Not Found
         Err(DockerClientError::UnexpectedStatusCode(code)) if code == 404 => {
             warn!("Upstream sent 404 Not Found");
             return Ok(StatusCode::NOT_FOUND.into_response())
@@ -99,62 +132,15 @@ pub async fn proxy_fetch_manifest(
         Err(e) => return Err(e.into())
     };
 
-    // Check if we have the same copy of the manifest somewhere in our files before sending a GET request
-    // to the upstream respository.
-    let proxy_manifest_hash_path = RegistryPathsHelper::manifest_path(&app.conf.proxy_storage, &container_ref, &proxy_manifest_head.hash);
-    info!("Check if cached manifest file exists for hash {}", proxy_manifest_head.hash);
-
-    if !proxy_manifest_hash_path.is_file() {
-        info!("File does not exist. Querying and caching the upstream manifest");
-        tokio::fs::create_dir_all(&proxy_manifest_hash_path.parent().unwrap()).await?;
-        // We don't have the manifest, GET the manifest referenced by the hash sent by the server
-        // and dump it into a file in our file system, no matter the original client request method.
-
-        // This time, if an error occurred, we don't care about the status code. If the registry sent a 200
-        // for a HEAD request *AND* sent the hash, why would it suddently declare the manifest not found or other status
-        // code ? Same goes for the hash.
-        let mut proxy_manifest = client.query_manifest(&proxy_manifest_head.hash, false).await?;
-        let mut proxy_manifest_file = tokio::fs::File::create(&proxy_manifest_hash_path).await?;
-        info!("Writing cache");
-        while let Some(chunk) = proxy_manifest.raw_response.chunk().await? {
-            proxy_manifest_file.write_all(&chunk).await?;
-        }
-
-        if !manifest_ref.starts_with("sha256") {
-            let proxy_manifest_tag_path = RegistryPathsHelper::manifest_path(&app.conf.proxy_storage, &container_ref, &manifest_ref);
-            // If the client requests a particular hash manifest, it will exist since we have already dumped it into its own file
-            // before copying it to the named tag.
-            tokio::fs::copy(&proxy_manifest_hash_path, &proxy_manifest_tag_path).await?;
-        }
-
-        info!("Writing metadata");
-        // Create all the files related to metadata saved in the proxy repository
-        let proxy_manifest_meta_hash_path = RegistryPathsHelper::manifest_meta(&app.conf.proxy_storage, &container_ref, &proxy_manifest_head.hash);
-        tokio::fs::create_dir_all(proxy_manifest_meta_hash_path.parent().unwrap()).await?;
-        let manifest_metadata = ManifestMetadata { content_type: &proxy_manifest.content_type, hash: &proxy_manifest.hash };
-        let manifest_metadata = serde_json::to_string(&manifest_metadata).unwrap();
-        let mut manifest_metadata_file = tokio::fs::File::create(&proxy_manifest_meta_hash_path).await?;
-        manifest_metadata_file.write_all(manifest_metadata.as_bytes()).await?;
-        manifest_metadata_file.flush().await?;
-        drop(manifest_metadata_file);
-
-        if !manifest_ref.starts_with("sha256") {
-            let proxy_manifest_meta_tag_path = RegistryPathsHelper::manifest_meta(&app.conf.proxy_storage, &container_ref, &manifest_ref);
-            tokio::fs::copy(&proxy_manifest_meta_hash_path, &proxy_manifest_meta_tag_path).await?;
-        }
-    } else {
-        // Else ­— if the hash file exists —, we do nothing.
-        info!("Hash is already cached")
-    }
-
+    let proxy_manifest_hash_path = RegistryPathsHelper::manifest_path(&app.conf.proxy_storage, &container_ref, &manifest_ref);
     let body = StreamBody::new(ReaderStream::new(tokio::fs::File::open(&proxy_manifest_hash_path).await?));
 
     Ok((
         StatusCode::OK,
         [
-            ("Content-Type", proxy_manifest_head.content_type.clone()),
-            ("Docker-Content-Digest", proxy_manifest_head.hash.clone()),
-            ("Content-Length", proxy_manifest_head.content_length.to_string())
+            ("Content-Type", content_type.clone()),
+            ("Docker-Content-Digest", proxy_hash.clone()),
+            ("Content-Length", content_length.to_string())
         ],
         body
     ).into_response())
